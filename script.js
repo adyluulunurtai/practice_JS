b7a374684e6e271537409fabd61f262d717a3aaa
Nurtai <adyluulunurtai@gmail.com>
2022-01-17 08:53:44 +0300

    praktik js metody dom poluchenie elementov so str

---
+++ script.js
@@ -0,0 +1,11 @@
+
+
+let box = document.getElementById("box");
+console.log(box);
+let btns = document.getElementsByTagName("button");
+console.log(btns);
+let hearts = document.querySelectorAll(".heart");
+console.log(hearts);
+hearts.forEach(item=>{
+    console.log(item)
+});
