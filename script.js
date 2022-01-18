0d6d5cfb1fda7f1ed42187eae8df4de7e168d847
Nurtai <adyluulunurtai@gmail.com>
2022-01-18 19:53:24 +0300

    rabota s elementami na str

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 
 
 let box = document.getElementById("box");
-console.log(box);
+let circles = document.getElementsByClassName("circle");
 let btns = document.getElementsByTagName("button");
-console.log(btns);
+let heart = document.querySelector(".heart");
 let hearts = document.querySelectorAll(".heart");
+
+//box.style.backgroundColor = "blue";
+//box.style.width = '300px';
+
+box.style.cssText = "background-color:blue; width:800px";
+btns[1].style.borderRadius = '100%';
+circles[0].style.cssText = "background-color:red ; width: 100px";
+
 console.log(hearts);
 hearts.forEach(item=>{
-    console.log(item)
+    item.style.backgroundColor = "black";
 });
